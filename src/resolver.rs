a25c659da0868713f491ea0693d087437b63915c
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-13 12:17:47 +0200

    *

---
+++ src/resolver.rs
@@ -0,0 +1,11 @@
+pub fn resolve_package(package_name: &str, version: &str) {
+  // TODO: resolve package
+}
+
+// collectDepsPackageList
+//
+//
+
+pub fn collect_dependencies_package_list(package_name: &str, version: &str) {
+  // TODO: collect dependencies package list
+}
