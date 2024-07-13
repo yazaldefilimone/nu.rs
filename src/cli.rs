eec1178c4ebd6751d0674c954ac290a466fabe19
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-13 18:07:21 +0200

    *

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
-use clap::{Arg, ArgAction, Command};
+#![allow(unused)]
+
+use clap::{arg, Arg, ArgAction, Command};
 
 pub fn nu_cli() -> Command {
   let nu_command = Command::new("nu")
@@ -13,96 +15,10 @@ pub fn nu_cli() -> Command {
           Arg::new("package")
             .help("Packages to install")
             .action(ArgAction::Set)
+            .required(true)
             .num_args(1..),
         )
-        .arg(
-          Arg::new("dev")
-            .help("Save as a development dependency")
-            .short_alias('D')
-            .action(ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("types")
-            .help("Include @types packages as dev dependencies")
-            .short_alias('T')
-            .action(ArgAction::SetTrue),
-        ),
-    )
-    .subcommand(
-      Command::new("remove")
-        .about("Remove packages")
-        .arg(
-          Arg::new("package")
-            .help("Packages to remove")
-            .action(ArgAction::Set)
-            .num_args(1..),
-        )
-        .arg(
-          Arg::new("dev")
-            .help("Remove as a development dependency")
-            .short_alias('D')
-            .action(ArgAction::SetTrue),
-        ),
-    )
-    .subcommand(
-      Command::new("update")
-        .about("Update packages")
-        .arg(
-          Arg::new("package")
-            .help("Packages to update")
-            .action(ArgAction::Set)
-            .num_args(1..),
-        )
-        .arg(
-          Arg::new("dev")
-            .help("Update as a development dependency")
-            .short_alias('D')
-            .action(ArgAction::SetTrue),
-        ),
-    )
-    .subcommand(
-      Command::new("upgrade")
-        .about("Upgrade packages")
-        .arg(
-          Arg::new("package")
-            .help("Packages to upgrade")
-            .action(ArgAction::Set)
-            .num_args(1..),
-        )
-        .arg(
-          Arg::new("dev")
-            .help("Upgrade as a development dependency")
-            .short_alias('D')
-            .action(ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("types")
-            .help("Include @types packages as dev dependencies")
-            .short_alias('T')
-            .action(ArgAction::SetTrue),
-        ),
-    )
-    .subcommand(
-      Command::new("list")
-        .long_flag("list")
-        .about("List installed packages")
-        .arg(
-          Arg::new("package")
-            .help("Packages to upgrade")
-            .action(ArgAction::Set)
-            .num_args(1..),
-        ),
-    )
-    .subcommand(
-      Command::new("search")
-        .long_flag("search")
-        .about("Search for package on npm registry")
-        .arg(
-          Arg::new("package")
-            .help("Package to search for")
-            .action(ArgAction::Set)
-            .num_args(1..),
-        ),
+        .arg(arg!(-D --dev "Save as a development dependency").required(false)),
     );
   return nu_command;
 }
